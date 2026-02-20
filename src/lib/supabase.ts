e9aef6ed8c7155295cd3138f6faa557811b85936
homsfalcon96-beep <homs.falcon.96@gmail.com>
2026-02-20 19:50:35 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.https://alkreorzcgvtnllbfknz.supabase.co
-const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsa3Jlb3J6Y2d2dG5sbGJma256Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzExNjI3OTgsImV4cCI6MjA4NjczODc5OH0.juR53i3c_6KnBvOyx_iZv-tyAaHC_yeNb4qhsriEeVE
+export const supabase = createClient(
+  'https://alkreorzcgvtnllbfknz.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsa3Jlb3J6Y2d2dG5sbGJma256Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzExNjI3OTgsImV4cCI6MjA4NjczODc5OH0.juR53i3c_6KnBvOyx_iZv-tyAaHC_yeNb4qhsriEeVE'
+)
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabaseAdmin = createClient(
+  'https://alkreorzcgvtnllbfknz.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsa3Jlb3J6Y2d2dG5sbGJma256Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MTE2Mjc5OCwiZXhwIjoyMDg2NzM4Nzk4fQ.0gkmr1JPfdkQVqYEpvt52LDCSocJdvzWKaWSiFiR068'
+)
