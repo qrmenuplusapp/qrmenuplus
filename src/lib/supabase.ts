d38ff61635b428f0f58b8f71e94e8d91b977957a
homsfalcon96-beep <homs.falcon.96@gmail.com>
2026-02-20 19:45:08 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,13 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.https://alkreorzcgvtnllbfknz.supabase.co
+const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsa3Jlb3J6Y2d2dG5sbGJma256Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzExNjI3OTgsImV4cCI6MjA4NjczODc5OH0.juR53i3c_6KnBvOyx_iZv-tyAaHC_yeNb4qhsriEeVE
 
-// للاستخدام العام (المنيو للزوار)
+// للاستخدام العام فقط
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
-// للداشبورد - يُستخدم فقط في API Routes (server-side)
-export const supabaseAdmin = createClient(
-  supabaseUrl,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-)
