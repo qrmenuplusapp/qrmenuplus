d247c941c41781bb28f03678981c5004a00a123c
qrmenuplusapp <qrmenuplusapp@gmail.com>
2026-02-25 12:28:50 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://alkreorzcgvtnllbfknz.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsa3Jlb3J6Y2d2dG5sbGJma256Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzExNjI3OTgsImV4cCI6MjA4NjczODc5OH0.juR53i3c_6KnBvOyx_iZv-tyAaHC_yeNb4qhsriEeVE'
+  'https://njgxkydxyipqliqdcqzz.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qZ3hreWR4eWlwcWxpcWRjcXp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwMDI0MzUsImV4cCI6MjA4NzU3ODQzNX0.qiFD13yyDlw-b7-Bwse0ra9jBL-SP1dMNRIa6Udvz7o'
 )
 
 export const supabaseAdmin = createClient(
-  'https://alkreorzcgvtnllbfknz.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsa3Jlb3J6Y2d2dG5sbGJma256Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MTE2Mjc5OCwiZXhwIjoyMDg2NzM4Nzk4fQ.0gkmr1JPfdkQVqYEpvt52LDCSocJdvzWKaWSiFiR068'
+  'https://njgxkydxyipqliqdcqzz.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qZ3hreWR4eWlwcWxpcWRjcXp6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MjAwMjQzNSwiZXhwIjoyMDg3NTc4NDM1fQ.KxhQiJ80pMANW-5Oio5llyjsZTUJElgDFmIJu_zcoJw'
 )
